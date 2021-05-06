267a902b055644759f1033ef829974ad980092de
rafael-projects <rss4110@hotmail.com>
2021-05-06 20:47:47 -0300

    Project create

---
+++ backend/src/main/java/com/devsuperior/DsVendas/DsVendasApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.DsVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsVendasApplication.class, args);
+	}
+
+}
