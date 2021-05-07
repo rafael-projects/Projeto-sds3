99c5bce2e2ed88056a85b9f2ca9a3dfe4c705d62
rafael-projects <rss4110@hotmail.com>
2021-05-07 03:21:39 -0300

    Paginations

---
+++ backend/src/main/java/com/devsuperior/DsVendas/repositores/SaleRepository.java
@@ -0,0 +1,11 @@
+package com.devsuperior.DsVendas.repositores;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.DsVendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+
+
+}
--- backend/src/main/java/com/devsuperior/DsVendas/repositores/SellerRepository.java
+++ backend/src/main/java/com/devsuperior/DsVendas/repositores/SellerRepository.java
@@ -1,7 +1,5 @@
 package com.devsuperior.DsVendas.repositores;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.DsVendas.entities.Seller;
