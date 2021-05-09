7507664303dcb8dc21fd95e20e342e49de4c57d9
rafael-projects <rss4110@hotmail.com>
2021-05-08 22:16:14 -0300

    Group by sarch

--- backend/src/main/java/com/devsuperior/DsVendas/service/SaleService.java
+++ backend/src/main/java/com/devsuperior/DsVendas/service/SaleService.java
@@ -1,6 +1,8 @@
 /*implementaçaõ básica de um serviço que depende de um repository,tendo um metodo para buscar yodos no repository*/
 package com.devsuperior.DsVendas.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,6 +10,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.DsVendas.dto.SaleDTO;
+import com.devsuperior.DsVendas.dto.SaleSuccessDTO;
+import com.devsuperior.DsVendas.dto.SaleSumDTO;
 import com.devsuperior.DsVendas.entities.Sale;
 import com.devsuperior.DsVendas.repositores.SaleRepository;
 import com.devsuperior.DsVendas.repositores.SellerRepository;
@@ -27,5 +31,18 @@ public class SaleService {
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
 	}
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+		return repository.successGroupedBySeller();
+	}
+	
+
+	
+
 	
 }
